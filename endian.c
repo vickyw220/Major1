89ec3c938f21a1a432655b6267fa7920a1ba9768
vickyw220 <vickywainaina@my.unt.edu>
2025-06-21 15:18:18 -0500

    created endia.c file

---
+++ endian.c
@@ -0,0 +1,9 @@
+// endian.c
+#include "major1.h"
+
+unsigned int endian_swap(unsigned int x) {
+    return ((x & 0x000000FF) << 24) |  // change the byte to 0 - 3
+           ((x & 0x0000FF00) << 8)  |  // change the byte to 1 - 2
+           ((x & 0x00FF0000) >> 8)  |  // change the byte to 2 -1
+           ((x & 0xFF000000) >> 24);   // change the byte to 3 - 0
+}
