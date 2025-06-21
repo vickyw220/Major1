a350b7d1b42547dfeca8f9f33a2971aedac367ed
Blarg2134 <muhammud.elsell@gmail.com>
2025-06-21 18:36:24 -0500

    Add files via upload

---
+++ clz.c
@@ -0,0 +1,11 @@
+#include "major1.h" 
+int count_leading_zeros(unsigned int x) {
+    int count = 0;
+    for (int i = 31; i >= 0; --i) {
+        if ((x >> i) & 1) {
+            break;
+        }
+        count++;
+    }
+    return count;
+}
